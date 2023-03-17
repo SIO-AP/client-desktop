204cf02450d6a4ca28d462f47cc2225f64674a1e
KeFroX <ponson.nathan.pro@gmail.com>
2023-03-17 14:10:05 +0100

    Gestion utilisateur

--- src/control/PnlGameMode.java
+++ src/control/PnlGameMode.java
@@ -5,14 +5,12 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 import controller.Controller;
-import view.ConsoleGUI;
 
 public class PnlGameMode extends JPanel {
 
